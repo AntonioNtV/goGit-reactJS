0f8b3bfa05e5b25191be9273d957ba958b51b7b1
AntonioNtV <antoniontv18@gmail.com>
2020-09-02 18:18:40 -0300

    Fazendo mudança de rotas com o Link do react-router-dom

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,5 +1,6 @@
 import React, { useState, FormEvent, useEffect } from 'react';
 import { FiChevronRight } from 'react-icons/fi';
+import { Link } from 'react-router-dom';
 import {
   Title, Form, Repositories, Error,
 } from './styles';
@@ -74,7 +75,11 @@ const Dashboard: React.FC = () => {
 
       <Repositories>
         {repositories.map((repository) => (
-          <a href="teste" key={repository.full_name}>
+          <Link
+            to={`/repository/${repository.full_name
+            }`}
+            key={repository.full_name}
+          >
             <img
               src={repository.owner.avatar_url}
               alt={repository.owner.login}
@@ -84,7 +89,7 @@ const Dashboard: React.FC = () => {
               <p>{repository.description}</p>
             </div>
             <FiChevronRight size={20} />
-          </a>
+          </Link>
         ))}
 
       </Repositories>
